997eaf4b237a0cdebdd66ad490d87c3e5374a724
hoosan31320 <hoos31320@gmail.com>
2021-02-27 00:45:51 +0700

    Add Delete Mutation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # movieql
 
 # Movie API with graphql
+
+# db.js에서 getById()에 filter(movie => movie.id === id); String(id) 필요없는 이유 확인해 보도록.
+
+# db.js에서 people일 때는 String(id) 필요했음. 차이가 무엇인지.
--- graphql/db.js
+++ graphql/db.js
@@ -39,7 +39,7 @@ export const addMovie = (name, score) => {
 };
 
 export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if (movies.length > cleanedMovies.length) {
         movies = cleanedMovies;
         return true;
